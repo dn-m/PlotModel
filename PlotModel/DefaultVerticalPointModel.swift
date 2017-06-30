06cd51ffdad58fd1e8fef15b7229527136c4bd71
James <jbean46@gmail.com>
2017-06-30 15:42:22 +0200

    Add DefaultVerticalPlot(Model,Point)

---
+++ PlotModel/DefaultVerticalPointModel.swift
@@ -0,0 +1,18 @@
+//
+//  DefaultVerticalPointModel.swift
+//  PlotModel
+//
+//  Created by James Bean on 6/30/17.
+//
+//
+
+public struct DefaultVerticalPointModel: PointModel {
+    
+    public typealias Entity = Double
+    
+    public var value: Double
+    
+    public init(_ value: Double) {
+        self.value = value
+    }
+}
--- PlotModel/PointModel.swift
+++ PlotModel/PointModel.swift
@@ -14,16 +14,21 @@ public protocol PointModel {
     
     /// The thing the needs to be converted into vertical dimension
     associatedtype Entity
+//    
+//    /// For example, `Staff` implements this as `StaffSlot`.
+//    associatedtype VerticalCoordinate
+//    
+//    /// Type that describes the abstract horizontal coordinate system of a `PlotView`.
+//    associatedtype HorizontalCoordinate
+//    
+//    /// Type that converts a given type of musical element to `AbstractVerticalPosition`.
+//    associatedtype VerticalAxis: Axis
+//    
+//    /// Type that converts a given type of musical element to `AbstractHorizontalPosition`.
+//    associatedtype HorizontalAxis: Axis
+}
+
+public protocol VerticalPointModel {
     
-    /// For example, `Staff` implements this as `StaffSlot`.
-    associatedtype VerticalCoordinate
     
-    /// Type that describes the abstract horizontal coordinate system of a `PlotView`.
-    associatedtype HorizontalCoordinate
-    
-    /// Type that converts a given type of musical element to `AbstractVerticalPosition`.
-    associatedtype VerticalAxis: Axis
-    
-    /// Type that converts a given type of musical element to `AbstractHorizontalPosition`.
-    associatedtype HorizontalAxis: Axis
 }
